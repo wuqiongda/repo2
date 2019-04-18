9738be956d253d93342aa997b43d064ef0fec393
lds3526373155 <3526373155@qq.com>
2019-04-18 12:34:53 +0800

    six!提交成功

---
+++ src/main/java/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * fileName:Hello
+ * description:
+ * author:刘大顺
+ * createTime:2019/4/18 12:33
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
