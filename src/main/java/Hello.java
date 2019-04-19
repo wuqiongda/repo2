1178a13812efd077564c14642a58589ccea94b50
lds3526373155 <3526373155@qq.com>
2019-04-19 15:20:16 +0800

    成功！3

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -6,7 +6,7 @@
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello ,world，lllllll33333");
+        System.out.println("hello 666666,world，lllllll33333");
         System.out.println("添加了一行修改成功！！");
     }
 }
